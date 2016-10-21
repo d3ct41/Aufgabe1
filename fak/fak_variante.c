0035a7cf82151d4705ea9ac92f16bd540429462c
d3ct41 <d3ct41@gmail.com>
2016-10-21 09:26:23 +0200

    kommentare eingefügt

--- fak/fak_variante.c
+++ fak/fak_variante.c
@@ -2,24 +2,31 @@
 
 int main()
 {
+	//Variabeln deklarieren
 	int ergebnis;
 	int n;
 	int zaehler;
-
+	
+	//Eingabe von n
 	printf("Von welcher Zahl soll die Fakuktaet gebildet werden?\n");
 	scanf("%d", &n);
 
+	//ergebnis und zaehler initalisieren
 	ergebnis = 1;
 	zaehler = 1;
 	
+	//solange zaehler kleine als n
 	while (zaehler <= n)
 	{
-		ergebnis = ergebnis * zaehler;
+		ergebnis = ergebnis * zaehler;	//ergebnis * zaehler
 		
-		zaehler = zaehler + 1;
+		zaehler = zaehler + 1;			//zaehler hochzaehlen
 	}
 	
+	//ausgabe ergebnis
 	printf("Ergebnis: %d \n", ergebnis);
+	
+	
 	return 0;
 }
 
--- math/math_funktion.c
+++ math/math_funktion.c
@@ -2,18 +2,22 @@
 
 int main()
 {
+	//deklaration von ergebnis, x, y
 	int ergebnis;
 	int x = 0;
 	int y = 0;
-
+	
+	//eingabe von x, (initalisierung
 	printf("Eingabe fuer x: ");
 	scanf("%d", &x);
-	
+	//eingabe von y (initalisierung
 	printf("Eingabe fuer y: ");
 	scanf("%d", &y);
 	
+	//ergebnis berechnung
 	ergebnis = x*x + y*y;
 	
+	//ausgabe vom ergebnis
 	printf("Ergebnis: %d ", ergebnis);
 	
 	return 0;
